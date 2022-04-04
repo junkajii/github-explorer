4488215018aeed6be530c0cfa4e03c032f709c7c
Jun Kajii <junk4jii@gmail.com>
2022-04-04 20:46:44 -0300

    :tada: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import RepositoryList from './components/RepositoryList';
+
+export default function App() {
+  return (
+  <div>
+    <h1>Hello ReactJS</h1>
+    <RepositoryList />
+  </div>
+  )
+}
---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import App from './App'
+import './styles/global.sass'
+
+ReactDOM.render(<App />, document.getElementById('root'))
