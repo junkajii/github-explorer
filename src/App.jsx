3e3b259577fcf4567c3e8fe3d9ab582a49b3ccd6
Jun Kajii <junk4jii@gmail.com>
2022-04-06 17:59:21 -0300

    :fire: remove counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import Counter from './components/Counter';
 import RepositoryList from './components/RepositoryList';
 
 export default function App() {
-  return (
-    <>
-      <h1>Hello ReactJS</h1>
-      <RepositoryList />
-      <Counter />
-    </>
-  )
+  return <RepositoryList />
 }
